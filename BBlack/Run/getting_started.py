00efc1f4799051b4b7370ca9026e4a40f3466dcf
CPerigois <caroleperigois@outlook.com>
2024-02-01 17:51:31 +0100

    Addition of slides

--- BBlack/Run/getting_started.py
+++ BBlack/Run/getting_started.py
@@ -1,19 +1,18 @@
 import BBlack.Run.advanced_params as AP
 import sys
 import os
-import json
 
 sys.path.append('../')
 """----------------------TO FILL----------------------"""
 
 """             *** GENERIC PARAMETERS ***            """
 
-name_of_project_folder = 'Example'  # Careful, the name there HAS TO BE the SAME as the name of the folder
+name_of_project_folder = 'Example'
 redshift_range = 15
 catalog_size = 200  # Minimum suggested 3000, 200 used for the example
 observables = ['Mc', 'q', 'z', 'chieff', 'chip']  # Choose among ['Mc', 'q', 'z', 'chip', 'chieff']
 n_cpu_max = 4  # Number maximal of cpu used by the code
-co_type = 'NSBH'  # To be chosen amon 'BBH', 'BNS', 'NSBH'
+co_type = 'BBH'  # To be chosen amon 'BBH', 'BNS', 'NSBH'
 from_cosmorate = True  # If your files are standard output from cosmorate this parameter should be True, else, should be
 # ... False
 param_dictionary = {'name_of_project_folder': name_of_project_folder,
@@ -57,8 +56,8 @@ observing_runs = ['O1', 'O2', 'O3a', 'O3b']
 """
         Choose option for bayesian analysis
 """
-rerun_bayesian_analysis = True
-observable_variation = {'all': {'observables': observables},
+rerun_bayesian_analysis = False
+observable_variation = {'all': {'observables': ['Mc', 'q', 'z','chip', 'chieff']},
                         'no_spin': {'observables': ['Mc', 'q', 'z']}}  # Choose among ['Mc', 'q', 'z', 'chip', 'chieff']
 compute_multi_channel = {
     '1VS2': [astromodel_1['name'], astromodel_2['name']]}  # To be kept empty to disable channel analysis
